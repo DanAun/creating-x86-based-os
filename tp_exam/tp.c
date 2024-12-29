5887a1ff47eaa4eca35078b46dd3149b8b74ef77
Daniel Aunan <23170040+DanAun@users.noreply.github.com>
2024-12-29 11:50:24 +0100

    Setup flat segmentation and started working on paging

--- tp_exam/tp.c
+++ tp_exam/tp.c
@@ -1,6 +1,108 @@
-/* GPLv2 (c) Airbus */
 #include <debug.h>
+#include <segmem.h>
+#include <pagemem.h>
+#include <cr.h>
+
+#define c0_idx  1
+#define d0_idx  2
+#define c3_idx  3
+#define d3_idx  4
+#define ts_idx  5
+
+#define c0_sel  gdt_krn_seg_sel(c0_idx)
+#define d0_sel  gdt_krn_seg_sel(d0_idx)
+#define c3_sel  gdt_usr_seg_sel(c3_idx)
+#define d3_sel  gdt_usr_seg_sel(d3_idx)
+#define ts_sel  gdt_krn_seg_sel(ts_idx)
+
+#define gdt_flat_dsc(_dSc_,_pVl_,_tYp_)                                 \
+   ({                                                                   \
+      (_dSc_)->raw     = 0;                                             \
+      (_dSc_)->limit_1 = 0xffff;                                        \
+      (_dSc_)->limit_2 = 0xf;                                           \
+      (_dSc_)->type    = _tYp_;                                         \
+      (_dSc_)->dpl     = _pVl_;                                         \
+      (_dSc_)->d       = 1;                                             \
+      (_dSc_)->g       = 1;                                             \
+      (_dSc_)->s       = 1;                                             \
+      (_dSc_)->p       = 1;                                             \
+   })
+
+#define tss_dsc(_dSc_,_tSs_)                                            \
+   ({                                                                   \
+      raw32_t addr    = {.raw = _tSs_};                                 \
+      (_dSc_)->raw    = sizeof(tss_t);                                  \
+      (_dSc_)->base_1 = addr.wlow;                                      \
+      (_dSc_)->base_2 = addr._whigh.blow;                               \
+      (_dSc_)->base_3 = addr._whigh.bhigh;                              \
+      (_dSc_)->type   = SEG_DESC_SYS_TSS_AVL_32;                        \
+      (_dSc_)->p      = 1;                                              \
+   })
+
+#define c0_dsc(_d) gdt_flat_dsc(_d,0,SEG_DESC_CODE_XR)
+#define d0_dsc(_d) gdt_flat_dsc(_d,0,SEG_DESC_DATA_RW)
+#define c3_dsc(_d) gdt_flat_dsc(_d,3,SEG_DESC_CODE_XR)
+#define d3_dsc(_d) gdt_flat_dsc(_d,3,SEG_DESC_DATA_RW)
+
+#define PDT_ADDR 0x600000
+#define PTB_ADDR 0x601000
+#define PT1_ADDR PTB_ADDR + 0x1000 * 1
+#define PT2_ADDR PTB_ADDR + 0x1000 * 2
+
+seg_desc_t GDT[6];
+tss_t      TSS;
+
+void init_gdt() {
+   gdt_reg_t gdtr;
+
+   GDT[0].raw = 0ULL;
+
+   c0_dsc( &GDT[c0_idx] );
+   d0_dsc( &GDT[d0_idx] );
+   c3_dsc( &GDT[c3_idx] );
+   d3_dsc( &GDT[d3_idx] );
+
+   gdtr.desc  = GDT;
+   gdtr.limit = sizeof(GDT) - 1;
+   set_gdtr(gdtr);
+
+   set_cs(c0_sel);
+
+   set_ss(d0_sel);
+   set_ds(d0_sel);
+   set_es(d0_sel);
+   set_fs(d0_sel);
+   set_gs(d0_sel);
+}
+
+void process1(){
+	while(1){};
+}
+
+void process2(){
+	while(1){};
+}
 
 void tp() {
-	// TODO
+	init_gdt();
+
+	//Set up TSS
+
+	//Set up IDT
+
+	//Set up paging
+	pde32_t *pdt = (pde32_t*) PDT_ADDR;
+	pte32_t *pt1 = (pte32_t*) PT1_ADDR;
+	set_cr3((uint32_t)pdt);
+
+	for (int i = 0; i < 1024; i++) {
+		pg_set_entry(&pt1[i], PG_KRN | PG_RW, i);
+	}
+
+	memset((void*)pdt, 0, PAGE_SIZE);
+	pg_set_entry(&pdt[0], PG_KRN|PG_RW, page_get_nr(pt1));
+
+	//Enable paging
+	uint32_t cr0 = get_cr0();
+	set_cr0(cr0|CR0_PG);
 }
