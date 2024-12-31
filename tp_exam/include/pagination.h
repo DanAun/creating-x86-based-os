b8c5077a8028d6f96367949bd5cd536adf3da24e
aittaleb <aittaleb@insa-toulouse.fr>
2024-12-31 05:46:31 +0100

    add important macros for pagination

--- tp_exam/include/pagination.h
+++ tp_exam/include/pagination.h
@@ -6,6 +6,14 @@
 #include "types.h"
 #include <debug.h>
 
+#define PGD_ADDR 0x600000
+#define PTB_ADDR_BASE 0x601000
+#define PTB_COUNT 3
+#define PTB_SIZE 0x1000
+
+#define ENTRIES_PER_PTE 0x400
+#define PG_SIZE 0x1000
+
 void map_addresses(pde32_t *pgd ,uint32_t v_start, uint32_t p_start, uint32_t size) ;
 
 #endif // DEBUG
